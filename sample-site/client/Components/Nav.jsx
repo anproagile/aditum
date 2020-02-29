efae948110eef2ea1786d275a8107ffdcd3a1b1b
anproagile <anproagile@gmail.com>
2020-02-29 15:50:02 -0500

    Write Enzyme test for FocusWrapper HOC

--- sample-site/client/Components/Nav.jsx
+++ sample-site/client/Components/Nav.jsx
@@ -5,13 +5,13 @@ function Nav() {
   return (
     <nav>
       <ul className="nav-links">
-        <Link className="linkColor" className='accessNavLinks' to="/">
+        <Link className="linkColor" to="/">
           <li>Home</li>
         </Link>
-        <Link className="linkColor" className='accessNavLinks' to="/about">
+        <Link className="linkColor" to="/about">
           <li>About</li>
         </Link>
-        <Link className="linkColor" className='accessNavLinks' to="/store">
+        <Link className="linkColor" to="/store">
           <li>Store</li>
         </Link>
         {/* <Link className="linkColor">
